71b66c32bf03ba32c5687d45fedaa6803525b0b2
NayanVR <nvradadiya281@gmail.com>
2022-10-02 20:53:10 +0530

    Added hover tooltip to skills logos

--- public/script.js
+++ public/script.js
@@ -5,27 +5,28 @@ import HScroll from './scroll.js';
 let nav = document.querySelector('.nav')
 let navToggle = document.querySelector('.nav__toggle')
 let liquidContainer = document.querySelector('.liquid-blobs');
-let navLinks = document.querySelectorAll(".nav__links a");
 let windowWidth = window.innerWidth;
+// let cursor = document.querySelector('.cursor');
 
-//Navbar toggle 
-navToggle.addEventListener('click', () => {    
+//CURSOR LOGIC
+// let lastYPos = 0;
+// document.addEventListener('mousemove', e => {
+//     cursor.style.top = `${e.pageY}px`;
+//     cursor.style.left = `${e.pageX}px`;
+//     lastYPos = e.pageY;
+// })
+
+// window.addEventListener('scroll', e => {
+//     cursor.style.top = `${window.scrollY + lastYPos}px`;
+// })
+
+//NAVBAR TOGGLE
+navToggle.addEventListener('click', () => {
     let isOpen = nav.getAttribute('data-is-open')
     nav.setAttribute('data-is-open', isOpen === 'true' ? 'false' : 'true')
 })
 
-// SCROLL SMOOTH FOR NAV LINKS
-
-navLinks.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth"
-      });
-    });
-});
-
-//Liquid Logic
+//LIQUID LOGIC
 let noOfBlobs = parseInt(windowWidth / 5);
 console.log(noOfBlobs);
 while (noOfBlobs-- > 0) {
@@ -35,7 +36,7 @@ while (noOfBlobs-- > 0) {
 function createBlob() {
     let blob = document.createElement('div');
     blob.classList.add('blob');
-    blob.style.animationDuration = `${getRandomNum(5,15)}s`;
+    blob.style.animationDuration = `${getRandomNum(5, 15)}s`;
     let blobSize = getRandomNum(2, 4);
     blob.style.height = `${blobSize}rem`;
     blob.style.width = `${blobSize}rem`;
@@ -46,13 +47,13 @@ function createBlob() {
 
 // HORIZONTAL SCROLL LOGIC
 
-let containers = document.getElementsByClassName('hscroll-container'); 
+let containers = document.getElementsByClassName('hscroll-container');
 
 for (let i = 0; i < containers.length; i++) {
     const container = containers[i];
-    
-    new HScroll(container, workData.UI);
-    
+
+    new HScroll(container, workData[Object.keys(workData)[i]]);
+
     container.addEventListener("mouseover", (e) => {
         disableScroll();
     });
@@ -69,9 +70,15 @@ image.src = 'assets/Figma logo.svg';
 
 skillsData.Skills.forEach(skill => {
     let image = document.createElement('img');
+    let div = document.createElement('div');
+    let p = document.createElement('p');
     image.src = skill.src;
     image.alt = skill.name;
-    skillsContainer.appendChild(image);
+    p.innerText = skill.name.replace(' Logo', '');
+    div.classList.add('skills__logo');
+    div.appendChild(image);
+    div.appendChild(p);
+    skillsContainer.appendChild(div);
 })
 
 
@@ -83,16 +90,16 @@ skillsData.Skills.forEach(skill => {
 let supportsPassive = false;
 try {
     window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-        get: function () { supportsPassive = true; } 
+        get: function () { supportsPassive = true; }
     }));
-} catch(e) {}
+} catch (e) { }
 
 let wheelOpt = supportsPassive ? { passive: false } : false;
 let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 function enableScroll() {
     window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
     window.removeEventListener('touchmove', preventDefault, wheelOpt);
     window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
@@ -109,7 +116,7 @@ function preventDefault(e) {
 }
 
 function preventDefaultForScrollKeys(e) {
-    let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+    let keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
     if (keys[e.keyCode]) {
         preventDefault(e);
         return false;
--- public/workData.js
+++ public/workData.js
@@ -2,37 +2,96 @@ export default {
     "UI": [
         {
             "name": "UI 1",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/e-commerce.png",
             "link": "www.google.com"
         },
         {
             "name": "UI 2",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/apple%20news.png",
             "link": "www.youtube.com"
         },
         {
             "name": "UI 3",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/grocery.png",
             "link": "www.twitter.com"
         },
         {
             "name": "UI 4",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/fitness.png",
             "link": "www.twitter.com"
         },
         {
             "name": "UI 5",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/contact.png",
             "link": "www.twitter.com"
         },
         {
             "name": "UI 6",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Ui%20Work%20Thumb/yoga.png",
+            "link": "www.twitter.com"
+        }
+    ],
+    "Logo": [
+        {
+            "name": "LUI 1",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Logo%20Work%20Thumb/sneak.png",
+            "link": "www.google.com"
+        },
+        {
+            "name": "UI 2",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Logo%20Work%20Thumb/eagle.png",
+            "link": "www.youtube.com"
+        },
+        {
+            "name": "UI 3",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Logo%20Work%20Thumb/pikake.png",
+            "link": "www.twitter.com"
+        },
+        {
+            "name": "UI 4",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Logo%20Work%20Thumb/bicycle.png",
+            "link": "www.twitter.com"
+        },
+        {
+            "name": "UI 5",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Logo%20Work%20Thumb/bounce.png",
+            "link": "www.twitter.com"
+        }
+    ],
+    "Graphic": [
+        {
+            "name": "GUI 1",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/watches.png",
+            "link": "www.google.com"
+        },
+        {
+            "name": "UI 2",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/tcf%20banner.png",
+            "link": "www.youtube.com"
+        },
+        {
+            "name": "UI 3",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/shoes.png",
+            "link": "www.twitter.com"
+        },
+        {
+            "name": "UI 4",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/harsh.png",
+            "link": "www.twitter.com"
+        },
+        {
+            "name": "UI 5",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/burger.png",
+            "link": "www.twitter.com"
+        },
+        {
+            "name": "UI 6",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/7pm.png",
             "link": "www.twitter.com"
         },
         {
             "name": "UI 7",
-            "imgurl": "./img.png",
+            "imgurl": "https://bxfhlymjhbqlrsdrfhms.supabase.co/storage/v1/object/public/images/Design%20Work%20Thumb/poha.png",
             "link": "www.twitter.com"
         }
     ]
